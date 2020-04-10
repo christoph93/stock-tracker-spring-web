3c2378c41d19d0a04f1b9df127195dac85ffb48b
Christoph Califice <christoph.califice@hotmail.com>
2020-04-09 21:34:31 -0300

    first commit

---
+++ src/test/java/cc/stock/tracker/StockTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.stock.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
